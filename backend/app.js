672959e5e0514354f43b489e70904127a820422f
Udara <uartcreations@gmail.com>
2024-09-04 00:19:56 +0530

    Remove log

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const app = express();
 //Middleware
 app.use(express.json())
 app.use("/Users", userRouter)
-app.use("/Employee", employeeRouter)//Add Employee routes
-console.log('Hello');
+app.use("/Employee", employeeRouter)
+
 //Database connection
 mongoose.connect(config.get('db.uri'))
     .then(()=> console.log('Connected to MongoDB...'))
