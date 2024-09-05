b4b6699efcf9c888073f0ea2c02ed74709aa870c
Gangani <ganganidewmini11@gmail.com>
2024-09-05 13:48:21 +0530

    add recycling product part(Controller, Route, Modle, app

---
+++ backend/Model/RecyclingProductModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recyclingProductSchema = new Schema({
+  //Create data for form
+  recyclingProductName:{
+    type:String,//Data Type
+    required:true,//validate
+  },
+  quantity:{
+    type:Number,//Data Type
+    required:true,//validate
+  },
+  stage:{
+    type:String,//Data Type "CLEANING, SORTING, MELTING"
+    required:true,//validate
+  },
+  status:{
+    type:String,//Data Type "DONE, INPROGRESS, REJECT"
+    required:true,//validate
+  },
+},{ collection: 'recyclingProduct' });
+
+const RecyclingProduct = mongoose.model('RecyclingProduct', recyclingProductSchema);
+
+module.exports = RecyclingProduct;
