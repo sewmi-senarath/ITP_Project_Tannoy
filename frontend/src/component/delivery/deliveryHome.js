35d901e44d2123904f6791f12930a68afda015ec
Methsara20 <dimalsham00@gmail.com>
2024-09-21 10:17:35 +0530

    add some features

--- frontend/src/component/delivery/deliveryHome.js
+++ frontend/src/component/delivery/deliveryHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../../styles/deliveryHome.css"; // Import the CSS file
+import "../../styles/deliveryHome.css" // Import the CSS file
 
 
 const DeliveryHome = () => {
