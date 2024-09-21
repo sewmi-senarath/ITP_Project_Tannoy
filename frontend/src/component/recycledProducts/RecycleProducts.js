4b3711938ca5e8de65ef32c916d603822d706b36
GanganiDewmini <ganganidewmini11@gmail.com>
2024-09-21 10:37:36 +0530

    Add recycle product dashboard and tables

--- frontend/src/component/recycledProducts/RecycleProducts.js
+++ frontend/src/component/recycledProducts/RecycleProducts.js
@@ -5,28 +5,20 @@ const RecycleProducts = () => {
     
 
   return (
-<<<<<<< HEAD
     <>
-=======
->>>>>>> 72053426c7e9a976bc75a5347fdad3c3f90504ca
     <div id='product-dashbord'>
         <div id='RecycleProductsDashBoard'>
            <div id='RecycleProductsLeftDiv'>
                 <div>
                     <p className='RecycleProductsLeftDashboardTitle'>Product and Recycling Dashboard</p> 
                     <a href='/recycling-products' id='RecyclingProductsButton-a'><button id='RecyclingProductsButton'>Recycling Products</button></a>
-<<<<<<< HEAD
                     <a href='/recycled-products' id='RecycledProductsButton-a'><button id='RecycledProductsButton'>Recycled Products</button></a>
-=======
-                    <a href='/recycling-products' id='RecycledProductsButton-a'><button id='RecycledProductsButton'>Recycleld Products</button></a>
->>>>>>> 72053426c7e9a976bc75a5347fdad3c3f90504ca
                     <div id='image-div'>
                     </div>
                 </div>
             </div>
             <div id='RecycleProductsRightDiv'>
                 <ul>
-<<<<<<< HEAD
                     <a href="/recycled-products"><ui>Complete</ui></a>
                     <a href="/recycling-products"><ui>In Progress</ui></a>
                     <a href="/recycling-products"><ui>Quality Good</ui></a>
@@ -39,28 +31,11 @@ const RecycleProducts = () => {
                     <a href="/recycling-products"><ui>4</ui></a>
                     <a href="/recycling-products"><ui>0</ui></a>
                     <a href="/recycling-products"><ui>0</ui></a>
-=======
-                    <a href=""><ui>Complete</ui></a>
-                    <a href=""><ui>In Progreess</ui></a>
-                    <a href=""><ui>Qulity Good</ui></a>
-                    <a href=""><ui>Qurlity Medium</ui></a>
-                    <a href=""><ui>Qulity Low</ui></a>
-                </ul>
-                <ul>
-                    <a href=""><ui>5</ui></a>
-                    <a href=""><ui>4</ui></a>
-                    <a href=""><ui>4</ui></a>
-                    <a href=""><ui>0</ui></a>
-                    <a href=""><ui>0</ui></a>
->>>>>>> 72053426c7e9a976bc75a5347fdad3c3f90504ca
                 </ul>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
   </>
-=======
->>>>>>> 72053426c7e9a976bc75a5347fdad3c3f90504ca
   )
 }
 
