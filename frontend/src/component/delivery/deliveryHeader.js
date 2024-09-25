34ae673b7de6a22b8af065652f3f19a3a37e67d1
Methsara20 <dimalsham00@gmail.com>
2024-09-26 01:10:05 +0530

    add total count of delivery req

--- frontend/src/component/delivery/deliveryHeader.js
+++ frontend/src/component/delivery/deliveryHeader.js
@@ -11,7 +11,7 @@ const Sidebar = () => (
         <li><a href="/deliveryHome">Home</a></li>
         <li><a href="/parsel-list">Tracking</a></li>
         <li><a href="/Add-parsel">Add Delivery Request</a></li>
-        <li><a href="/delivery">Delivery Team</a></li>
+        <li><a href="/cusParsel-list">Customer Order List</a></li>
         <li><a href="/login">Log Out</a></li>
         <li><a href="/register">Register</a></li>
       </ul>
