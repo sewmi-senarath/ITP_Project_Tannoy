1a2801126229a7bd0f083a4ac785cc3393f3739f
Methsara20 <dimalsham00@gmail.com>
2024-09-28 15:41:35 +0530

    css

--- frontend/src/component/delivery/deliveryHeader.js
+++ frontend/src/component/delivery/deliveryHeader.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../../component/delivery/Header.css'; // Assuming the same CSS is used for layout
 
 const Sidebar = () => (
-  <div className="sidebar">
-    <div className="sidebar-logo">
+  <div id="sidebar">
+    <div id="sidebar-logo">
       <h2>Tannoy Electricals</h2> 
     </div>
-    <nav className="sidebar-nav">
+    <nav id="sidebar-nav">
       <ul>
         <li><a href="/deliveryHome">Home</a></li>
         <li><a href="/parsel-list">Tracking</a></li>
